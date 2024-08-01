e6d3b6e634d3f1dd4caaeee149639ed30bb90222
hyunjuleee <kangje0610@gmail.com>
2024-08-01 14:42:17 +0900

    Comment 출력 방법 비교

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Article
+from .models import Article, Comment
 from .forms import ArticleForm, CommentForm
 # Create your views here.
 def index(request):
@@ -13,9 +13,12 @@ def index(request):
 def detail(request, id):
     article = Article.objects.get(id=id)
     form = CommentForm()
+
+    comments = Comment.objects.filter(article_id=id) #
     context = {
         'article': article,
         'form': form,
+        'comments': comments, #
     }
     return render(request, 'detail.html', context)
 
